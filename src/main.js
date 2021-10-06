14164c77076ddb6e5289d094c5bf8504015f73d0
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2021-10-06 13:01:25 +0100

    improve palette override
    - better naming for style element
    - defer script
    - append style to document head

--- src/main.js
+++ src/main.js
@@ -1,26 +1,26 @@
 const hideChangePaletteButton = function () {
-  if (document.getElementById('pft-hide-change-palette') !== null) {
+  if (document.getElementById('palette-override') !== null) {
     return;
   }
 
   const toRunInPageContext = function () {
     const changePaletteLabel = window.tumblr.languageData.translations['Change Palette'] || 'Change Palette';
     const textContent = `button[aria-label="${changePaletteLabel}"] { display: none !important; }`;
-    const style = Object.assign(document.createElement('style'), { textContent, id: 'pft-hide-change-palette' });
-    document.documentElement.append(style);
+    const style = Object.assign(document.createElement('style'), { textContent, id: 'palette-override' });
+    document.head.append(style);
   };
 
   const scriptWithNonce = [...document.scripts].find(script => script.getAttributeNames().includes('nonce'));
   const nonce = scriptWithNonce.nonce || scriptWithNonce.getAttribute('nonce');
 
   const injectable = `(${toRunInPageContext.toString()})()`;
-  const script = Object.assign(document.createElement('script'), { nonce, textContent: injectable });
+  const script = Object.assign(document.createElement('script'), { nonce, defer: true, textContent: injectable });
   document.documentElement.append(script);
   script.remove();
 };
 
 const showChangePaletteButton = function () {
-  const style = document.getElementById('pft-hide-change-palette');
+  const style = document.getElementById('palette-override');
   if (style) style.remove();
 };
 
