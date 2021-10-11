e3f1664c9e90247219fad70051d992a11bb06d2e
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2021-10-11 16:42:29 +0100

    render custom palettes in popup

--- src/browser_action/render_palettes.js
+++ src/browser_action/render_palettes.js
@@ -1,3 +1,5 @@
+const fromCamelCase = string => string.replace(/[A-Z]/g, match => ` ${match}`).replace(/^./, match => match.toUpperCase());
+
 const getInstalledPalettes = async function () {
   const url = browser.runtime.getURL('/palettes.json');
   const file = await fetch(url);
@@ -29,6 +31,22 @@ const renderPalettes = async function () {
       option.selected = paletteName === currentPalette;
       optgroup.append(option);
     }
+  }
+
+  const storageObject = await browser.storage.local.get();
+  const definedPalettes = Object.keys(storageObject).filter(key => key.startsWith('palette:'));
+  if (definedPalettes.length === 0) return;
+
+  const optgroup = document.createElement('optgroup');
+  optgroup.label = 'Custom';
+  paletteSelect.append(optgroup);
+
+  for (const paletteKey of definedPalettes) {
+    const option = document.createElement('option');
+    option.value = paletteKey;
+    option.textContent = fromCamelCase(paletteKey.split(':')[1]);
+    option.selected = paletteKey === currentPalette;
+    optgroup.append(option);
   }
 };
 
