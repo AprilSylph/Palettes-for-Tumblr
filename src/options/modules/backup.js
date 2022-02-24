5c3abb3b9875e494d5e99d0212e2cf16ae84a654
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2022-02-24 14:35:37 +0000

    move backup-related functions to module

---
+++ src/options/modules/backup.js
@@ -0,0 +1,97 @@
+import { getDatestamp, isValidDate } from './datetime.js';
+
+const loadFileButton = document.getElementById('load-file');
+const loadFileInput = loadFileButton.nextElementSibling;
+
+const selectAllButton = document.getElementById('select-all');
+const selectNoneButton = document.getElementById('select-none');
+const exportList = document.getElementById('export-list');
+const saveFileButton = document.getElementById('save-file');
+
+const loadFromFile = async ({ currentTarget }) => {
+  try {
+    const { files } = currentTarget;
+    if (files.length === 0) return;
+
+    const [importedPalettes] = files;
+
+    if (importedPalettes.type !== 'application/json') {
+      throw new Error('Invalid file type selected.');
+    }
+
+    const paletteString = await importedPalettes.text();
+    const paletteData = JSON.parse(paletteString);
+
+    const paletteEntries = Object.entries(paletteData);
+    const validPaletteEntries = paletteEntries.filter(([paletteKey]) => {
+      const keyParts = paletteKey.split(':');
+      if (keyParts.length !== 3) return false;
+
+      const [type, name, timestamp] = keyParts;
+      return type === 'palette' && /\s/.test(name) === false && isValidDate(parseInt(timestamp));
+    });
+
+    if (validPaletteEntries.length === 0) throw new Error('No palettes found in file.');
+
+    await browser.storage.local.set(Object.fromEntries(validPaletteEntries));
+    window.alert(`Successfully imported ${validPaletteEntries.length} palettes!`);
+  } catch (exception) {
+    window.alert(exception.toString());
+  } finally {
+    currentTarget.value = currentTarget.defaultValue;
+  }
+};
+
+const selectAllForExport = () => {
+  [...exportList.options].forEach(option => { option.selected = true; });
+  exportList.focus();
+  updateSaveFileButton();
+};
+
+const selectNoneForExport = () => {
+  [...exportList.options].forEach(option => { option.selected = false; });
+  exportList.focus();
+  updateSaveFileButton();
+};
+
+const updateSaveFileButton = () => {
+  saveFileButton.disabled = [...exportList.options].every(({ selected }) => selected === false);
+};
+
+const saveToFile = async () => {
+  const isFullBackup = [...exportList.options].every(({ selected }) => selected === true);
+
+  const storageKeys = [...exportList.options]
+    .filter(({ selected }) => selected === true)
+    .map(({ value }) => value);
+
+  if (storageKeys.length === 0) return;
+
+  const paletteData = await browser.storage.local.get(storageKeys);
+  const paletteString = JSON.stringify(paletteData, null, 2);
+  const paletteBlob = new Blob([paletteString], { type: 'application/json' });
+
+  const href = URL.createObjectURL(paletteBlob);
+  const download = isFullBackup
+    ? `Palettes Backup @ ${getDatestamp()}`
+    : storageKeys.length === 1
+      ? storageKeys[0].split(':')[1]
+      : window.prompt('Enter a name for this palette collection:');
+
+  if (download === null) return;
+
+  const anchor = Object.assign(document.createElement('a'), { href, download: `${download}.json` });
+  anchor.click();
+  URL.revokeObjectURL(href);
+};
+
+loadFileButton.addEventListener('click', () => loadFileInput.click());
+loadFileInput.addEventListener('change', loadFromFile);
+
+selectAllButton.addEventListener('click', selectAllForExport);
+selectNoneButton.addEventListener('click', selectNoneForExport);
+exportList.addEventListener('change', updateSaveFileButton);
+saveFileButton.addEventListener('click', saveToFile);
+updateSaveFileButton();
+
+export { exportList };
--- src/options/ui.js
+++ src/options/ui.js
@@ -1,5 +1,6 @@
+import { exportList } from './modules/backup.js';
 import { hexToRgb, rgbToHex } from './modules/color.js';
-import { getDatestamp, getTimestamp, isValidDate } from './modules/datetime.js';
+import { getTimestamp } from './modules/datetime.js';
 
 const { getURL } = browser.runtime;
 const getBuiltInPaletteList = fetch(getURL('/palettes.json')).then(response => response.json());
@@ -18,14 +19,6 @@ const saveButton = document.getElementById('save');
 const deleteButton = document.getElementById('delete');
 
 const previewSection = document.getElementById('preview');
-
-const loadFileButton = document.getElementById('load-file');
-const loadFileInput = loadFileButton.nextElementSibling;
-
-const selectAllButton = document.getElementById('select-all');
-const selectNoneButton = document.getElementById('select-none');
-const exportList = document.getElementById('export-list');
-const saveFileButton = document.getElementById('save-file');
 
 const paletteForm = document.getElementById('palette-form');
 const createdTime = paletteForm.querySelector('time');
@@ -147,83 +140,6 @@ const updatePreview = () => {
   formEntries.forEach(([property, value]) => previewSection.style.setProperty(`--${property}`, value));
 };
 
-const loadFromFile = async ({ currentTarget }) => {
-  try {
-    const { files } = currentTarget;
-    if (files.length === 0) return;
-
-    const [importedPalettes] = files;
-
-    if (importedPalettes.type !== 'application/json') {
-      throw new Error('Invalid file type selected.');
-    }
-
-    const paletteString = await importedPalettes.text();
-    const paletteData = JSON.parse(paletteString);
-
-    const paletteEntries = Object.entries(paletteData);
-    const validPaletteEntries = paletteEntries.filter(([paletteKey]) => {
-      const keyParts = paletteKey.split(':');
-      if (keyParts.length !== 3) return false;
-
-      const [type, name, timestamp] = keyParts;
-      return type === 'palette' && /\s/.test(name) === false && isValidDate(parseInt(timestamp));
-    });
-
-    if (validPaletteEntries.length === 0) throw new Error('No palettes found in file.');
-
-    await browser.storage.local.set(Object.fromEntries(validPaletteEntries));
-    window.alert(`Successfully imported ${validPaletteEntries.length} palettes!`);
-  } catch (exception) {
-    window.alert(exception.toString());
-  } finally {
-    currentTarget.value = currentTarget.defaultValue;
-  }
-};
-
-const selectAllForExport = () => {
-  [...exportList.options].forEach(option => { option.selected = true; });
-  exportList.focus();
-  updateSaveFileButton();
-};
-
-const selectNoneForExport = () => {
-  [...exportList.options].forEach(option => { option.selected = false; });
-  exportList.focus();
-  updateSaveFileButton();
-};
-
-const updateSaveFileButton = () => {
-  saveFileButton.disabled = [...exportList.options].every(({ selected }) => selected === false);
-};
-
-const saveToFile = async () => {
-  const isFullBackup = [...exportList.options].every(({ selected }) => selected === true);
-
-  const storageKeys = [...exportList.options]
-    .filter(({ selected }) => selected === true)
-    .map(({ value }) => value);
-
-  if (storageKeys.length === 0) return;
-
-  const paletteData = await browser.storage.local.get(storageKeys);
-  const paletteString = JSON.stringify(paletteData, null, 2);
-  const paletteBlob = new Blob([paletteString], { type: 'application/json' });
-
-  const href = URL.createObjectURL(paletteBlob);
-  const download = isFullBackup
-    ? `Palettes Backup @ ${getDatestamp()}`
-    : storageKeys.length === 1
-      ? storageKeys[0].split(':')[1]
-      : window.prompt('Enter a name for this palette collection:');
-
-  if (download === null) return;
-
-  const anchor = Object.assign(document.createElement('a'), { href, download: `${download}.json` });
-  anchor.click();
-  URL.revokeObjectURL(href);
-};
-
 newSelect.addEventListener('change', createNewPalette);
 openSelect.addEventListener('change', onPaletteSelected);
 deleteButton.addEventListener('click', deleteCurrentPalette);
@@ -239,15 +155,6 @@ getBuiltInPaletteList.then(builtInPaletteList => {
   }));
   newSelect.append(optgroup);
 });
-
-loadFileButton.addEventListener('click', () => loadFileInput.click());
-loadFileInput.addEventListener('change', loadFromFile);
-
-selectAllButton.addEventListener('click', selectAllForExport);
-selectNoneButton.addEventListener('click', selectNoneForExport);
-exportList.addEventListener('change', updateSaveFileButton);
-saveFileButton.addEventListener('click', saveToFile);
-updateSaveFileButton();
 
 paletteForm.addEventListener('reset', disableSaveButton);
 paletteForm.addEventListener('submit', onFormSubmitted);
