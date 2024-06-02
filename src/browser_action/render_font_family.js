272e51d448d4265b035f66c53befa5a8131eb05f
marcustyphoon <marcustyphoon@gmail.com>
2024-06-02 13:05:22 -0700

    implement custom font family text input (#141)
    Co-authored-by: April Sylph <28949509+AprilSylph@users.noreply.github.com>

--- src/browser_action/render_font_family.js
+++ src/browser_action/render_font_family.js
@@ -1,16 +1,28 @@
+const fontFamilySelect = document.getElementById('font-family');
+const customFontFamilyInput = document.getElementById('font-family-custom');
+
 const writeFontFamily = async function ({ target: { value } }) {
+  fontFamilySelect.dataset.value = value;
   browser.storage.local.set({ fontFamily: value });
+};
+
+const writeCustomFontFamily = async function ({ target: { value } }) {
+  browser.storage.local.set({ customFontFamily: value });
 };
 
 const renderFontFamily = async function () {
   const { fontFamily = '' } = await browser.storage.local.get('fontFamily');
-  const fontFamilySelect = document.getElementById('font-family');
+  const { customFontFamily = '' } = await browser.storage.local.get('customFontFamily');
 
   fontFamilySelect.addEventListener('input', writeFontFamily);
+  customFontFamilyInput.addEventListener('input', writeCustomFontFamily);
 
   [...fontFamilySelect.options].forEach(option => {
     option.selected = option.value === fontFamily;
   });
+  fontFamilySelect.dataset.value = fontFamily;
+
+  customFontFamilyInput.value = customFontFamily;
 };
 
 renderFontFamily();
