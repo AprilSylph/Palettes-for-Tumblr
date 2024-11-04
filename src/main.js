3c9596f09d34977d53b303002e4636a06e88081e
Marcus <marcustyphoon@gmail.com>
2024-11-04 13:38:23 -0800

    handle `--font-family-modern` property

--- src/main.js
+++ src/main.js
@@ -43,6 +43,10 @@ const applyFontFamily = async function () {
     '--font-family',
     fontFamily === 'custom' ? customFontFamily : fontFamily
   );
+  document.documentElement.style.setProperty(
+    '--font-family-modern',
+    fontFamily === 'custom' ? customFontFamily : fontFamily
+  );
 };
 
 const applyFontSize = async function () {
