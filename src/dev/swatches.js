0f5ab4d069e1ac289e9ed3bd9c7c673d304743f9
Marcus <marcustyphoon@gmail.com>
2025-05-02 16:29:29 -0700

    port swatch preview page

---
+++ src/dev/swatches.js
@@ -0,0 +1,65 @@
+import { getSemanticTokens } from '../get_semantic_tokens.js';
+import { dom } from './dom.js';
+
+const jsonFetch = (url) => fetch(browser.runtime.getURL(url)).then((response) => response.json());
+
+const builtInPaletteList = await jsonFetch('/palettes.json');
+const builtInPalettes = await jsonFetch('/palette_data.json');
+const builtInPaletteSystem = await jsonFetch('/palette_system_data.json');
+
+const containerElement = document.getElementById('container');
+
+const paletteIds = builtInPaletteList.flatMap(([label, options]) => options);
+
+const paletteKeys = [
+  ...Object.keys(builtInPalettes.trueBlue).map((key) =>
+    key === 'accent' ? 'deprecated-accent' : key
+  ),
+  ...Object.keys(builtInPaletteSystem.trueBlue)
+];
+
+for (const [id, label] of paletteIds) {
+  const nativeColumn = dom('div', { class: 'column' });
+  const previewColumn = dom('div', { class: 'column' });
+
+  nativeColumn.append(
+    dom('div', { class: 'swatch header' }, null, [`${label} (real)`]),
+    ...paletteKeys.map((key) =>
+      dom('div', { class: 'swatch', style: `background: var(--${key}, repeating-linear-gradient(45deg, rgb(255 0 0 / 0.5), rgb(255 0 0 / 0.5) 2px, transparent 4px, transparent 20px))` }, null, [key])
+    )
+  );
+  previewColumn.append(
+    dom('div', { class: 'swatch header' }, null, [label]),
+    ...paletteKeys.map((key) =>
+      dom('div', { class: 'swatch', style: `background: var(--${key}, repeating-linear-gradient(45deg, rgb(255 0 0 / 0.5), rgb(255 0 0 / 0.5) 2px, transparent 4px, transparent 20px))` }, null, [key])
+    )
+  );
+
+  let currentPaletteData = builtInPalettes[id];
+  if (currentPaletteData.accent && !currentPaletteData['deprecated-accent']) {
+    currentPaletteData = {
+      ...currentPaletteData,
+      'deprecated-accent': currentPaletteData.accent
+    };
+    delete currentPaletteData.accent;
+  }
+  const currentPaletteSystemData = builtInPaletteSystem[id];
+
+  Object.entries({ ...currentPaletteData, ...currentPaletteSystemData }).forEach(
+    ([property, value]) =>
+      nativeColumn.style.setProperty(
+        `--${property}`,
+        !value || value.startsWith('rgb') ? value : `rgba(${value})`
+      )
+  );
+
+  Object.entries({ ...currentPaletteData, ...getSemanticTokens(currentPaletteData) }).forEach(
+    ([property, value]) =>
+      previewColumn.style.setProperty(
+        `--${property}`,
+        !value || value.startsWith('rgb') ? value : `rgba(${value})`
+      )
+  );
+
+  containerElement.append(dom('div', { class: 'columns' }, null, [nativeColumn, previewColumn]));
+}
