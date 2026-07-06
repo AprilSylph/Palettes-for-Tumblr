d9ede37cbb03ec77c90d43015151822eb3935afd
marcustyphoon <marcustyphoon@gmail.com>
2026-07-06 05:44:17 -0700

    work around click util not working in firefox 151+

--- dev/update-palette-system-data.js
+++ dev/update-palette-system-data.js
@@ -13,7 +13,10 @@ try {
   await Promise.all(
     page.frames().map(async (frame) => {
       const privacyAgreeButton = await frame.$('.cmp__dialog-footer button.cmp-components-button.white-space-normal.is-primary');
-      privacyAgreeButton?.click();
+      privacyAgreeButton?.evaluate((element) => {
+        // let evaluate function call return cleanly before iframe context is invalidated
+        requestAnimationFrame(() => element.click());
+      });
     })
   );
   await page.waitForSelector(':root:not(:has(#cmp-app-container iframe))');
